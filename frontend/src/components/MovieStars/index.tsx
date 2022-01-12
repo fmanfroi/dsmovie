2e5332e15f11d8daf6821fe7841cf0e288252d5f
fernando.manfroi <fernando.manfroi@dataprev.gov.br>
2022-01-11 23:41:38 -0300

    movie cards stars pagination

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,16 @@
+import {ReactComponent as StarFull} from 'assets/starfull.svg'
+import {ReactComponent as StarHalf} from 'assets/starhalf.svg'
+import {ReactComponent as StarEmpty} from 'assets/starempty.svg'
+import './styles.css'
+function MovieStars() {
+    return (
+        <div className="dsmovie-stars-container">
+            <StarFull />
+            <StarFull />
+            <StarFull />
+            <StarHalf />
+            <StarEmpty />
+        </div>
+    );
+}
+export default MovieStars;
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,21 +1,21 @@
-import {ReactComponent as GithubIcon} from 'assets/github.svg';
+import { ReactComponent as GithubIcon } from 'assets/github.svg';
 import './styles.css'
 
 function Navbar() {
-  return (
-    <header>
-    <nav className="container">
-        <div className="dsmovie-nav-content">
-            <h1>DSMovie</h1>
-            <a href="https://github.com/fmanfroi" target="_blank" rel="noreferrer">
-                <div className="dsmovie-contact-container">
-                    <GithubIcon />
-                    <p className="dsmovie-contact-link">/fmanfroi</p>
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/fmanfroi" target="_blank" rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/fmanfroi</p>
+                        </div>
+                    </a>
                 </div>
-            </a>
-        </div>
-    </nav>
-</header>
-  );
+            </nav>
+        </header>
+    );
 }
 export default Navbar;
