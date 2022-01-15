2da5dec6310085ed738a4fc87a024416489ba53c
fernando.manfroi <fernando.manfroi@dataprev.gov.br>
2022-01-15 13:47:45 -0300

    pagination

--- backend/src/main/java/com/manfactory/dsmovie/service/MovieService.java
+++ backend/src/main/java/com/manfactory/dsmovie/service/MovieService.java
@@ -16,14 +16,14 @@ public class MovieService {
 	@Autowired
 	private MovieRepository repository;
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public Page<MovieDTO> findAll(Pageable pageable) {
 		Page<Movie> result = repository.findAll(pageable);
 		Page<MovieDTO> page = result.map(x -> new MovieDTO(x));
 		return page;			
 	}
 	
-	@Transactional
+	@Transactional(readOnly = true) 
 	public MovieDTO findById(Long id) {
 		Movie result = repository.findById(id).get();		
 		return new MovieDTO(result);			
